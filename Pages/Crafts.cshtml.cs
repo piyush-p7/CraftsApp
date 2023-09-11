e5083e6b098dbe3aa72167e8afce7462822861e5
piyush-p7 <piyushpandita151@gmail.com>
2023-09-11 15:01:51 +0530

    Added pages

---
+++ Pages/Crafts.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using ContosoCrafts.WebSite.Services;
+using Crafts_App.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JokesApp.Pages
+{
+    public class CraftsModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+        public JsonFileProductService ProductService { get; set; }
+        public IEnumerable<Product> Products { get; private set; }
+        public int num = 0;
+
+        public CraftsModel(ILogger<PrivacyModel> logger, JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+            num = 1;
+        }
+    }
+}
