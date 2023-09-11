cdb2eb8e6542d705ccb9de228132d891a621c741
piyush-p7 <piyushpandita151@gmail.com>
2023-09-11 14:56:05 +0530

    Added Controllers

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using ContosoCrafts.WebSite.Services;
+using Crafts_App.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Crafts_App.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return this.ProductService.GetProducts();
+        }
+
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get([FromQuery] string productID, [FromQuery] int newRating)
+        {
+            ProductService.AddRating(productID, newRating);
+            return Ok();
+        }
+    }
+}
